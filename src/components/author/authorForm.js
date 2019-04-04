ef8b9405d8e8fe55a28ec183eeb063d3cec0af37
oscarhirst <oscar1hirst@gmail.com>
2019-04-04 16:13:36 +0100

    Added form saving + validation

--- src/components/author/authorForm.js
+++ src/components/author/authorForm.js
@@ -4,20 +4,24 @@ var React = require('react');
 var TextInput = require('../common/textInput');
 
 var AuthorForm = React.createClass({
-    render: function () {
+    render: function() {
         return (
             <form>
                 <TextInput
                     name="firstName"
                     label="First Name"
                     value={this.props.author.firstName}
-                    onChange={this.props.onChange} />
+                    onChange={this.props.onChange}
+                    error={this.props.errors.firstName}
+                />
                 <TextInput
                     name="lastName"
                     label="Last Name"
                     value={this.props.author.lastName}
-                    onChange={this.props.onChange} />
-                <input type="submit" value="Save" className="btn btn-default" />
+                    onChange={this.props.onChange}
+                    error={this.props.errors.lastName}
+                />
+                <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave} />
             </form>
         );
     }
